ff43e58fca9e303e0a5c5bdd145b30224f510bc5
Sam Cook <129230960+koocimas@users.noreply.github.com>
2024-04-25 14:20:53 -0400

    Initial Commit

---
+++ RoveTravel/RoveTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoveTravelApp.swift
+//  RoveTravel
+//
+//  Created by Sam Cook on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct RoveTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
